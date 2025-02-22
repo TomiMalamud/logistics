ed1192cc7507af76b5119c410ea4b65d7c601c04
TomiMalamud <tomasmalamud@gmail.com>
2025-02-22 11:05:25 -0300

    feat: replace boxes animation

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -62,9 +62,8 @@ export function AppSidebar({ onSignOut, ...props }: AppSidebarProps) {
       icon: Bed,
       badge:
         role === "admin" && hasNewOrders ? (
-          <span className="relative flex h-2 w-2">
-            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
-            <span className="relative inline-flex rounded-full h-2 w-2 bg-red-500"></span>
+          <span className="relative flex animate-bounce">
+            <span className="relative inline-flex h-3 w-3 rounded-full bg-red-500"></span>
           </span>
         ) : null,
     },
--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -20,7 +20,7 @@ import * as React from "react";
 
 const SIDEBAR_COOKIE_NAME = "sidebar_state";
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
-const SIDEBAR_WIDTH = "13rem";
+const SIDEBAR_WIDTH = "14rem";
 const SIDEBAR_WIDTH_MOBILE = "18rem";
 const SIDEBAR_WIDTH_ICON = "3rem";
 const SIDEBAR_KEYBOARD_SHORTCUT = "b";
